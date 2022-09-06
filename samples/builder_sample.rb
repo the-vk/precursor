5257d54078a4ea836df2f0bc5cdf6fe61a2605d6
Andrew Maraev <the_vk@thevk.net>
2022-09-05 21:28:45 -0700

    add samples

---
+++ samples/builder_sample.rb
@@ -0,0 +1,16 @@
+# require 'precursor'
+
+require 'set'
+
+require_relative '../lib/precursor'
+
+config = Precursor.create do |builder|
+  builder.vault Precursor::EnvVault.new(allow_list: ['config__test'])
+
+  builder.key :'key_with_default' do |key_builder|
+    key_builder.default 42
+  end
+end
+
+pp config[:'config.test']
+pp config[:key_with_default]
