d1fdf5f1138cd5dc43d32b5ed7a1e5e1d64ad7b6
Andrew Maraev <the_vk@thevk.net>
2022-09-07 17:51:41 -0700

    add ArgvVault to handle command line args

---
+++ lib/argv_vault.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+require 'optparse'
+require 'optparse/date'
+require 'optparse/time'
+require 'optparse/uri'
+
+require_relative 'vault'
+
+module Precursor
+  # Vault that parses command line arguments
+  class ArgvVault < Vault
+    def initialize(argv)
+      @vault_data = {}
+      @parser = OptionParser.new do |parser|
+        parser.on('-h', '--help', 'Prints this help') do
+          puts parser
+          exit
+        end
+      end
+      yield self
+      @parser.parse(argv)
+    end
+
+    def key(key_name)
+      builder = ArgvKeyBuilder.new(key_name)
+      yield builder
+      builder.build(@parser, @vault_data)
+    end
+
+    protected
+
+    def store
+      @vault_data
+    end
+
+    class ArgvKeyBuilder
+      def initialize(key_name)
+        @key_name = key_name
+        @short = []
+        @long = []
+        @type = nil
+        @description = nil
+      end
+
+      def short(short_key)
+        @short.push(short_key)
+      end
+
+      def long(long_key)
+        @long.push(long_key)
+      end
+
+      def type(type)
+        @type = type
+      end
+
+      def description(desc)
+        @description = desc
+      end
+
+      def build(parser, vault_data)
+        args = @short + @long
+        args.push(@type) unless @type.nil?
+        args.push(@description) unless @description.nil?
+
+        parser.on(*args) do |v|
+          vault_data[@key_name] = v
+        end
+      end
+    end
+  end
+end
---
+++ samples/argv_sample.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require_relative '../lib/precursor'
+
+config = Precursor.create do |builder|
+  argv = ['-f', '--db-provider=MONGO', '--limit=3/4']
+
+  builder.key :flag do |kb|
+    kb.default false
+  end
+
+  builder.vault (Precursor::ArgvVault.new(argv) do |v|
+    v.key :'db.provider' do |kv|
+      kv.long '--db-provider=DBPROVIDER'
+      kv.description 'DB provider name'
+    end
+    v.key :force do |kv|
+      kv.short '-f'
+      kv.type TrueClass
+      kv.description 'Force action'
+    end
+    v.key :'fraction.limit' do |kv|
+      kv.long '--limit=LIMIT'
+      kv.type Numeric
+      kv.description 'Fractional limit'
+    end
+    v.key :flag do |kv|
+      kv.short '-a'
+      kv.type TrueClass
+      kv.description 'A flag'
+    end
+  end)
+end
+
+pp config[:'db.provider']
+pp config[:force]
+pp config[:'fraction.limit']
+pp config[:flag]
