bed39c739061f3f9ccfeb4578d8d7ac52edf9932
Andrew Maraev <the_vk@thevk.net>
2022-09-07 17:50:47 -0700

    move common code to Precursor::Vault

--- lib/vault.rb
+++ lib/vault.rb
@@ -5,14 +5,20 @@ module Precursor
   class Vault
     # Returns true if the vault has a value for the given key, otherwise false
     # @return [Boolean] true if the vault has a value for the given key, otherwise false
-    def key?
-      false
+    def key?(key)
+      store.key?(key)
     end
 
     # Returns a value fot the given key, nil if key is not available
     # @return [Object|Nil] a value for the given key
     def value(key)
-      nil
+      store[key]
+    end
+
+    protected
+    # Replace this method in descendant classes to return Hash-like store
+    def store
+      {}
     end
   end
 end
